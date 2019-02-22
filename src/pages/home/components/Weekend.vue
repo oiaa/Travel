77c914098130e0cb646603a6e747f06aa62d0e3e
chenrui <2035851466@qq.com>
2019-02-22 19:34:11 +0800

    add recommend weekend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <div class="title">周末去那儿</div>
+        <ul>
+            <li class="item border-bottom" v-for="(item, index) of recommendList" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img :src="item.imgUrl" class="item-img">
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeWeekend',
+    data() {
+        return {
+            recommendList: [{
+                id: '0001',
+                imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+                title: '故宫',
+                desc: '故宫故宫故宫故宫故宫故宫故宫故宫'
+            }, {
+                id: '0002',
+                imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+                title: '故宫',
+                desc: '故宫故宫故宫故宫故宫故宫故宫故宫'
+            }, {
+                id: '0003',
+                imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+                title: '故宫',
+                desc: '故宫故宫故宫故宫故宫故宫故宫故宫'
+            },]
+        }
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/mixins.styl'
+    .title
+        line-height: .8rem
+        background: #eee
+        text-indent: .2rem
+        margin-top: .2rem
+    .item-img-wrapper
+        overflow: hidden
+        height: 0
+        padding-bottom: 36.5%
+        .item-img
+            width: 100%
+    .item-info
+        padding: .1rem
+        .item-title
+            line-height: .54rem
+            font-size: .32rem
+            ellipsis()
+        .item-desc
+            line-height: .4rem
+            color: #ccc
+            ellipsis()
+</style>
