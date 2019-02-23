a841c1bf1da80c15162c366a98407a436b88647b
chenrui <2035851466@qq.com>
2019-02-23 15:52:47 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,65 @@
 <template>
-    <div class="search">
-        <input type="text" class="search-input" placeholder="请输入城市名或拼音">
+    <div>
+        <div class="search">
+            <input type="text" class="search-input" placeholder="请输入城市名或拼音" v-model="keyword">
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li v-for="item of list" class="search-item border-bottom" :key="item.id">
+                    {{item.name}}
+                </li>
+                <li class="search-item border-bottom" v-show="hasNoData">
+                    没有找到匹配数据
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+    props: {
+        cities: Object
+    },
+    computed: {
+        hasNoData () {
+            return !this.list.length
+        }
+    },
+    data() {
+        return {
+            keyword: '',
+            list: [],
+            timer: null
+        }
+    },
+    watch: {
+        keyword () {
+            if (this.timer) {
+                clearTimeout(this.timer)
+            }
+            if (!this.keyword) {
+                this.list = []
+                return
+            }
+            this.timer = setTimeout(() => {
+                const result = []
+                for (let i in this.cities) {
+                    this.cities[i].forEach((value) => {
+                        if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+                            result.push(value)
+                        }
+                    })
+                }
+                this.list = result
+            }, 100)
+        }
+    },
+    mounted() {
+        this.scroll = new Bscroll(this.$refs.search)
+    },
 }
 </script>
 
@@ -25,4 +78,18 @@ export default {
             border-radius: .06rem
             text-align: center
             color: #666
+    .search-content
+        position: absolute
+        top: 1.58rem
+        left: 0
+        right: 0
+        bottom: 0
+        overflow: hidden
+        z-index: 1
+        background: #eee
+        .search-item
+            line-height: .62rem
+            padding-left: .2rem
+            color: #666
+            background: #fff
 </style>
