44ecf62668eb8a3325214851864dc090e56e8d52
chenrui <2035851466@qq.com>
2019-02-23 08:32:46 +0800

    index finish

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wrapper">
-        <swiper :options="swiperOption">
-            <swiper-slide v-for="item of swiperList" :key="item.id">
+        <swiper :options="swiperOption" v-if="showSwiper">
+            <swiper-slide v-for="item of list" :key="item.id">
                 <img :src="item.imgUrl" class="swiper-img">
             </swiper-slide>
             
@@ -13,19 +13,21 @@
 <script>
 export default {
     name: 'HomeSwiper',
+    props: {
+        list: Array
+    },
     data() {
         return {
             swiperOption: {
                 pagination: '.swiper-pagination',
                 loop: true
             },
-            swiperList: [{
-                id: '0001',
-                imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/c64e6f7308f67f0862adb64b9d9fcff0.jpg_750x200_37c4c320.jpg'
-            }, {
-                id: '0002',
-                imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/9f73976e40c4ef845cabe0efc0269ebb.jpg_750x200_aab92b7a.jpg'
-            }]
+            
+        }
+    },
+    computed: {
+        showSwiper () {
+            return this.list.length
         }
     },
 }
